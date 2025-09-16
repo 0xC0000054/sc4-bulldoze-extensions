b6a5760fa485f42368ca9d008130563905dce005
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2025-09-16 15:55:23 -0600

    Fix a typo in the file header

--- src/NetworkOccupantFilterBase.h
+++ src/NetworkOccupantFilterBase.h
@@ -15,7 +15,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with SC4ClearPollution.
+ * along with sc4-bulldoze-extensions.
  * If not, see <http://www.gnu.org/licenses/>.
  */
 
